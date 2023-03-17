d856489a057fd4c481f8010fa28022a5a09bd3f1
jinyoung <sm099@naver.com>
2023-03-17 11:17:08 +0900

    [인프런] - 순열 구하기 오답노트

--- inflearn algorithm study/section8/순열 구하기/otherAnswer.js
+++ inflearn algorithm study/section8/순열 구하기/otherAnswer.js
@@ -1,23 +1,31 @@
-function solution(n, arr, r) {
-  let answer = [];
+function solution() {
   let ch = Array.from({ length: n }, () => 0);
-  let tmp = Array.from({ length: r }, () => 0);
+  let element = [];
+  let answer = [];
   function DFS(l) {
+    if (l > r) return;
     if (l === r) {
-      answer.push(tmp.slice());
+      let copy = [...element];
+      let size = copy.length;
+      if (size === r) {
+        answer.push(copy);
+      }
     } else {
       for (let i = 0; i < n; i++) {
         if (ch[i] === 0) {
           ch[i] = 1;
-          tmp[l] = arr[i];
+          element.push(arr[i]);
           DFS(l + 1);
+
           ch[i] = 0;
+          element.pop();
+          DFS(l + 1);
         }
       }
     }
   }
   DFS(0);
-  return answer;
+  console.log(answer);
 }
 
 console.log(solution(3, [3, 6, 9], 2));
